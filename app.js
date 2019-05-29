9e65eff142e51c222903f90ffb0d6e86784b737a
Frank Runfola <frunfola1229@gmail.com>
2019-05-29 12:49:17 +0000

    make it better

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var express     = require("express"),
     session = require("express-session"),
     seedDB      = require("./seeds"),
     methodOverride = require("method-override");
-    
+
+
 //requiring routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
@@ -62,7 +63,9 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+var port = (process.env.PORT || 3000);
+
+app.listen(port, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 	console.log(process.env.PORT);
 }); 
